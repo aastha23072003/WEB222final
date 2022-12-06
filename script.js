57396e85b6596671d4dea421a2396fce328381c3
aastha23072003 <119976846+aastha23072003@users.noreply.github.com>
2022-12-06 17:49:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+          $("#show").click(function () {
+            $("#Create").show();
+          });
+        });
+    document.getElementById()
+        
+
+    
